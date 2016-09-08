a2dd6066885b86503e5062b06b9eab6aac12254e
GAK00 <geran.kunz@gmail.com>
2016-09-08 09:50:59 -0600

    small update added a random element for fun

--- src/stupid/controller/Controller.java
+++ src/stupid/controller/Controller.java
@@ -1,19 +1,35 @@
 package stupid.controller;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Controller
 {
 	private String myName;
+	private Scanner inputReader;
+	private Random rand;
 
 	public Controller()
 	{
 		myName = "Geran.Kunz";
-
+		inputReader = new Scanner(System.in);
+		rand = new Random();
 	}
 
 	public void start()
 	{
 		System.out.println("This app is less stupid");
 		System.out.println(myName);
+		askQuestions();
+	}
+
+	private void askQuestions()
+	{
+		System.out.println("How is your day?");
+		String input = inputReader.nextLine();
+		System.out.println("Oh, you said: "+input+" about your day.");
+		System.out.println("its random time" + (rand.nextInt(10)+1));
+				
 	}
 
 }

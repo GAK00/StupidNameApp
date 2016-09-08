49c13b8ec9096081344cf172b947078767356f9e
GAK00 <geran.kunz@gmail.com>
2016-09-08 08:50:16 -0600

    Made app that says name

---
+++ src/stupid/controller/Controller.java
@@ -0,0 +1,19 @@
+package stupid.controller;
+
+public class Controller
+{
+	private String myName;
+
+	public Controller()
+	{
+		myName = "Geran.Kunz";
+
+	}
+
+	public void start()
+	{
+		System.out.println("This app is less stupid");
+		System.out.println(myName);
+	}
+
+}
---
+++ src/stupid/controller/Runner.java
@@ -0,0 +1,10 @@
+package stupid.controller;
+
+public class Runner {
+
+	public static void main(String[] args){
+		Controller control = new Controller();
+		control.start();
+	
+	}
+}
